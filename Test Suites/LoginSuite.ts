0336c0f42e62aeea1e8f5c1e4f5c4209cf90eff7
Giyu <giyu@Giyus-MacBook-Air.local>
2026-05-03 17:09:54 +0700

    nothing

--- Test Suites/LoginSuite.ts
+++ Test Suites/LoginSuite.ts
@@ -12,7 +12,7 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>e99579ad-60d8-4d56-899f-184d6fef4fb7</testSuiteGuid>
    <testCaseLink>
-      <guid>5c53cc27-1fd0-43f5-a271-9ff61731cced</guid>
+      <guid>98824b66-92f7-4e8e-8df2-a3e96ae977a4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/LoginGithub</testCaseId>
